4aa6f5ff6c3e5ebe1131afd17ee911463bd8ec8d
g5zn98 <gg5zn98@gmail.com>
2025-12-04 16:28:00 +0800

    新增走路音效及兩個房間

--- Assets/Scripts/MouseWalker.cs
+++ Assets/Scripts/MouseWalker.cs
@@ -6,6 +6,9 @@ public class MouseWalker : MonoBehaviour
     public float Speed = 5f;
     [Tooltip("Use Camera's Transform to indicate move direction")]
     public Transform CameraTransform;
+    private AudioClip footStep;
+    public float stepRate = 0.5f;
+	public float stepCoolDown;
 
     InputAction _toWalk;
     CharacterController _controller;
@@ -25,7 +28,15 @@ public class MouseWalker : MonoBehaviour
             Vector3 dir = CameraTransform.forward;
             dir.y = 0;
             dir = Speed * Time.deltaTime * dir.normalized;
-
+            stepCoolDown -= Time.deltaTime;
+		if (stepCoolDown < 0f){
+            footStep = Resources.Load("Sound/Footsteps_Tile_Walk_05") as AudioClip;
+            var source = GetComponent<AudioSource>();
+			source.pitch = 1f + Random.Range (-0.2f, 0.2f);
+			source.PlayOneShot (footStep, 0.9f);
+			stepCoolDown = stepRate;
+		}
+            
             _controller.Move(dir);
         }
     }
